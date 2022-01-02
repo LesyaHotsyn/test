e6a7ca93afb08f1f729ed080ccb19a8f8f561d3e
LesyaHotsyn <85835454+LesyaHotsyn@users.noreply.github.com>
2022-01-02 18:50:16 +0200

    I

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[] apples = { 1, 2, 3, 4, 5 };
+int[] apples2 = new int[5];
+List<int> apples3 = new List<int>();
+
+apples3.Add(1);
+apples3.Add(8);
+apples3.RemoveAt(0);
+Console.WriteLine(apples3.Count());
+foreach (int apple in apples3)
+{
+    Console.WriteLine(apple);
+}
+
+
